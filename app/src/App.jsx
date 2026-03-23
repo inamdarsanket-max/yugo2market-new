fafa281475f4eab4302d47d40fdf12533a5bfa9a
inamdarsanket-max <inamdar.sanket@gmail.com>
2026-03-23 16:04:23 +0530

    Update import paths for page components to include .jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
 function App() {
   return (
